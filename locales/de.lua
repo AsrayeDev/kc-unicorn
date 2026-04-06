6fcd928f2570a8f57f41962c9eb73aab568df164
Asraye <asrayeofficial@gmail.com>
2026-04-06 20:39:08 +1000

    i18n: german translations (and mild inconsistency fix)

---
+++ locales/de.lua
@@ -0,0 +1,15 @@
+Locale = Locale or {}
+
+Locale['de'] = { -- 'de' is the reference that will be used for 'Config.Language'
+	StandaloneLapText			= "Nach einem Lapdance fragen", 
+	LapText						= "Lapdance kaufen (~g~%$~w~)", 
+	BoughtLapdance				= "Du hast gerade einen Lapdance für %$ gekauft", 
+	StripperActive				= "Die Tänzerin ist bereits beschäftigt!", 
+	NotEnoughMoney				= "Du hast nicht genug Geld. Ein Lapdance kostet %$", 
+	AllPlacesTaken				= "Alle Plätze sind bereits belegt", 
+	lapStopped					= "Dein Lapdance wurde unterbrochen", 
+	Lean						= "Anlehnen", 
+	StandaloneLeanNotice		= "Drücke ~INPUT_CONTEXT~ um aufzustehen\nDrücke ~INPUT_JUMP~ um Geld zu werfen", 
+	LeanNotice					= "Drücke ~INPUT_CONTEXT~ um aufzustehen\nDrücke ~INPUT_JUMP~ um Geld zu werfen (~g~%$~w~)", 
+	NotEnoughCashLean			= "Nicht genug Bargeld zum Werfen vorhanden" 
+}
